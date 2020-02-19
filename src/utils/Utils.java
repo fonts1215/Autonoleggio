8a2c077afa6738497775eb587779ec6c1611e8f9
Andrea Fonte <fonts1215@gmail.com>
2020-02-19 14:26:21 +0100

    bug fixed in utils class

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,12 +1,11 @@
 package utils;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utils {
 	public static String dateFormatter(Date date) {
-		String format = new SimpleDateFormat();
+		SimpleDateFormat format = new SimpleDateFormat();
 		return format.format(date);
 	}
 }
